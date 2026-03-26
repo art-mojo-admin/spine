45ef9ee97bf3aebbe094f51b61909ce97a287ce0
art-mojo-admin <kpettit851@gmail.com>
2026-03-25 17:29:33 -0700

    fix: Add HTML env var replacement plugin for netlify dev mode
    %VITE_APP_NAME% in index.html was not replaced during dev,
    causing Vite to fail parsing HTML as JS module.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'html-env-replace',
+      transformIndexHtml(html) {
+        return html.replace(/%(\w+)%/g, (_, key) => process.env[key] ?? '')
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),           // Assembled src
